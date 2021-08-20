c792c7cee60c72eea119e3d56bda674f3e99db74
brjohns97 <31782272+brjohns97@users.noreply.github.com>
2021-08-19 21:32:40 -0500

    about to go to bed. final commit fixing up readme and update scripts

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Automatic Doorlock
 
-doorlock.py is run on a raspberrypi0w to intelligently and conveniently lock/unlock my front door
+[/program/doorlock.py](https://github.com/brjohns97/Automatic_doorlock/blob/master/program/doorlock.py) is run on a raspberrypi0w using buildroot linux to intelligently and conveniently lock/unlock my front door
--- update_local_unit.sh
+++ update_local_unit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 LOCAL_DIR="/c/Users/alexc/Documents/rpi-0-w/program/"
-LOCAL_FILE="doorlock.py"
+FILE1="doorlock.py"
+FILE2="watchdog.sh"
 REMOTE_IP_ADDRESS='192.168.1.69'
 REMOTE_DIR="/program/"
 USER="root"
@@ -9,5 +10,5 @@ USER="root"
 PASSWORD=`cat "C:\Users\alexc\Documents\rpi-0-w\remote_update_script\password.txt"`
 PUTTY_COMMAND="C:\Users\alexc\Documents\rpi-0-w\remote_update_script\putty_cmd.txt"
 
-pscp -pw $PASSWORD scp "$LOCAL_DIR$LOCAL_FILE" "$USER@$REMOTE_IP_ADDRESS:$REMOTE_DIR"
+pscp -pw $PASSWORD scp "$LOCAL_DIR$FILE1" "$LOCAL_DIR$FILE2" "$USER@$REMOTE_IP_ADDRESS:$REMOTE_DIR"
 putty -ssh "$USER@$REMOTE_IP_ADDRESS" -pw $PASSWORD -m $PUTTY_COMMAND
